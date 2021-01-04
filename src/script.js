bbe0daa664489f81696347069559d37814463a66
Elizabeth-Susan <74855891+Elizabeth-Susan@users.noreply.github.com>
2021-01-04 21:32:59 +0200

    Updated the time

--- src/script.js
+++ src/script.js
@@ -1,3 +1,28 @@
+//12 timestamp is the number of milliseconds that has happened since 1970 creating a function formatDate that receives timestamp
+function formatDate(timestamp) {
+  //14 going from a timestamp (long number) to a date like "Friday 05:00"
+  let now = new Date(timestamp);
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 //5 define the function that I want axios to call and response back from api
 function displayTemperature(response) {
   //6 we know axios puts the actual response in data and further details like city can be taken
@@ -10,12 +35,16 @@ function displayTemperature(response) {
   //10 humidity and wind via span id
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  //11 changing the day, hour and minutes to last updated with span and id
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   //9 capitalize via CSS the first letter of description
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  //13 in response.data it mentions an integer with the milliseconds since 1970 so multiply by 1000 gives current time and date
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 //1 copy the apiKey
 let apiKey = "a8bb545115365cdae986d0ebd7521ddb";
